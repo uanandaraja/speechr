af9369e576fc00ac785ab5e669bab703ce8f3c6a
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-27 00:25:37 +0700

    feat: add lokal

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import lokalTunnel from "lokal-webpack-plugin";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.plugins.push(
+        new lokalTunnel({
+          lanAddress: "next.local",
+          publicAddress: "next.i.lokal-so.site", // Optional: custom public address
+        }),
+      );
+    }
+    return config;
+  },
+};
 
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
