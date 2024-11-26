55dddf4e9d3c09a262acca29030689c1fae797d8
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-26 22:20:26 +0700

    fix: new env

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,8 @@ export interface Env {
   GOOGLE_CLIENT_ID: string;
   GOOGLE_CLIENT_SECRET: string;
   GOOGLE_REDIRECT_URI: string;
-  HABITKU_AUTH_KV: KVNamespace;
   CLIENT_REDIRECT_URL: string;
+  SPEECHR_AUTH_KV: string;
+  TTS_PROVIDER_URL: string;
+  TTS_CALLBACK_URL: string;
 }
