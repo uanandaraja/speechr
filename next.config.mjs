d2612a0054e20f6867989341c4ceb0b3ceace821
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-27 21:06:57 +0700

    remove lokal

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,7 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-import lokalTunnel from "lokal-webpack-plugin";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.plugins.push(
-        new lokalTunnel({
-          lanAddress: "next.local",
-          publicAddress: "next.i.lokal-so.site", // Optional: custom public address
-        }),
-      );
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
