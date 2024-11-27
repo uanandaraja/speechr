1850fd44e565c460be4d0109a9f2a2b8ff075ea0
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-28 00:03:13 +0700

    add image remote to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.buatcoverletter.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
